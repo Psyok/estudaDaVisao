fe32977eec78140c1d057d34f8f158bcfa8c6351
Psyok <nascimento.gabriel06@escola.pr.gov.br>
2026-08-07 09:05:19 -0300

    ok

--- scripy.js
+++ scripy.js
@@ -1,1 +1,26 @@
 //botoes de acessibilidade
+document.addEventListener("DOMContentLoaded, () =>"{
+
+const btnContraste = document.getElementById("btn-contrare");
+const btnAumentar = document.getElementById("btn-aumentar-texto");
+const btnDiminuir = document.getElementById("btn-dimibuir-texto")
+
+//aumente a fonte
+let tamanhoAltualFonte = 100;
+
+
+//função alto contraste
+
+btnContraste.addEventListener("click", () =>){
+
+    document.body.classlist.toogle("alto-contraste");
+
+//acessibilidade para leitores de tela
+
+const ativo = document.body.classList.contarins("alto-contraste
+    btnContraste.setAttribute("aria-pressed", ativo))
+
+
+
+}
+})
